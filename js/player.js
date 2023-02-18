5a983dbe75af18a1f3c56407a31c4c40b9bba08d
sarahjahan28 <sumaiya.jahan.sarah28@gmail.com>
2023-02-18 03:38:34 -0800

    add onclick

--- js/player.js
+++ js/player.js
@@ -1,1 +1,35 @@
-docu
+// document.getElementById('select-btn').addEventListener('click', function (playerId) {
+//     const olList = document.getElementById('ol-list')
+//     const nameId = document.getElementById(playerId)
+//     const nameEden = nameId.innerText
+//     const newList = document.createElement('li')
+//     newList.innerText = nameEden
+//     olList.appendChild(newList)
+
+// })
+
+
+// document.getElementById('select-btn').addEventListener('click', function () {
+
+//     const olList = document.getElementById('ol-list')
+//     // olList.target.parentNode.removeChild(olList.target)
+//     // console.log(olList.target)
+//     console.log(olList)
+
+// })
+
+
+
+function addPlayerName(playerId,) {
+    const olList = document.getElementById('ol-list')
+    const nameId = document.getElementById(playerId)
+    const nameEden = nameId.innerText
+    const newList = document.createElement('li')
+    newList.innerText = nameEden
+    olList.appendChild(newList)
+
+
+
+
+
+}
